52713e34dc7e42a362ed3684aaab777e6765e0b9
Sean Leonard <stleonar@usc.edu>
2016-11-15 21:09:31 -0800

    corrected some extraneuous text

--- public/js/main.js
+++ public/js/main.js
@@ -83,6 +83,19 @@ var app =angular
                                 $location.path( "/" );
                                 $route.reload();
                                 return -1;
+<<<<<<< Updated upstream
+=======
+=======
+                controllerAs: 'vm'
+            })
+            .when('/alerts', {
+                templateUrl: '/templates/alerts.html',
+                controller: 'alertCtrl',
+                controllerAs: 'vm'
+            });
+>>>>>>> Nick
+
+>>>>>>> Stashed changes
                             }else{
                                 return Object.values(data);
                             }
